b5af795432a523391bb782dc3f191e9f705f1921
JorgeRuizArocho <35469001+JorgeRuizArocho@users.noreply.github.com>
2020-02-19 10:27:02 -0600

    2/19/2020

--- myscripts/ANGSD_mypop.sh
+++ myscripts/ANGSD_mypop.sh
@@ -1,9 +1,10 @@
 ---
 title: "Untitled"
 author: "Jorge A. Ruiz-Arocho"
-date: "2/12/2020"
+date: "2/19/2020"
 output: html_document
 ---
+
 myrepo="/users/j/r/jruizaro/Ecological_Genomics_2020"
 
 mkdir ${myrepo}/myresults/ANGSD
@@ -39,3 +40,73 @@ ANGSD -b ${output}/${mypop}_bam.list \
 -doSaf 1 \
 -doHWE 1 \
 # -SNP_pval 1e-6
+
+# Now let's calculate the SFS for the folded minor allele spectra
+
+myrepo="/users/j/r/jruizaro/Ecological_Genomics_2020"
+
+mkdir ${myrepo}/myresults/ANGSD
+
+output="${myrepo}/myresults/ANGSD"
+
+mypop="WA"
+
+ls /data/project_data/RS_ExomeSeq/mapping/BWA/${mypop}_*sorted.rm*.bam >${output}/${mypop}_bam.list
+
+REF="/data/project_data/RS_ExomeSeq/ReferenceGenomes/Pabies1.0-genome_reduced.fa"
+
+# Estimating GL's and allele frequencies for all sites with ANGSD
+
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_folded_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1  
+
+# Get a rough first estmate of the SFS and then us a prior
+
+realSFS ${output}/${mypop}_folded_allsites.saf.idx -maxIter 1000 -tole 1e-6 -P 1 > ${output}/${mypop}_outFold.sfs
+
+# Get refined estimate of the SFS and doTheta
+
+ANGSD -b ${output}/${mypop}_bam.list \
+-ref ${REF} -anc ${REF} \
+-out ${output}/${mypop}_folded_allsites \
+-nThreads 1 \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 20 \
+-minQ 20 \
+-setMinDepth 3 \
+-minInd 2 \
+-setMinDepthInd 1 \
+-setMaxDepthInd 17 \
+-skipTriallelic 1 \
+-GL 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doSaf 1 \
+-fold 1 \
+-pest ${output}/${mypop}_outFold.sfs \
+-doThetas 1
+
+# Use the doTheta output from above to estimate nucleotide diversity
+
+thetaStat do_stat ${output}/${mypop}_folded_allsites.thetas.idx
